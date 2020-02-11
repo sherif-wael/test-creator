36f209afe999fee74bc72cac57e381da4d55db17
Sherif Wael <sherifwael80@gmail.com>
2020-02-12 00:10:54 +0200

    prepare to deploy

--- server.js
+++ server.js
@@ -26,9 +26,14 @@ app.use("/api/test", require("./routes/api/test"))
 // app.use(express.static("./client/public"));
 
 if(process.env.NODE_ENV == "production"){
-    app.use(express.static("./client/build"))
-}else{
-    app.use(express.static("./client/public"))
+    app.use(express.static("./client/build"));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+      });
+ }else{
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+      });
 }
 
 app.listen(process.env.PORT || 5000, () => console.log("server is listening to port 5000"))
